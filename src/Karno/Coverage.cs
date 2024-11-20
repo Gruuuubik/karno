514a6cbfb1160081d4f7bbc60d9caeee5cab4915
Pavel Zhuraulevich <p.zhuravlevich@gmail.com>
2024-11-20 23:15:31 +0100

    Replace spaces with tabs

--- src/Karno/Coverage.cs
+++ src/Karno/Coverage.cs
@@ -3,41 +3,41 @@ using System.Linq;
 
 namespace Karno
 {
-    public class Coverage : HashSet<Group>
-    {
+	public class Coverage : HashSet<Group>
+	{
 
-        public Coverage(IEnumerable<Group> collection) : base(collection)
-        {
+		public Coverage(IEnumerable<Group> collection) : base(collection)
+		{
 
-        }
+		}
 
-        public Coverage() : base()
-        {
+		public Coverage() : base()
+		{
 
-        }
+		}
 
-        public long? Cost { get; set; }
+		public long? Cost { get; set; }
 
-        #region Equality Comparison
+		#region Equality Comparison
 
-        public override bool Equals(object obj)
-        {
-            if (obj.GetType() != GetType())
-                return false;
+		public override bool Equals(object obj)
+		{
+			if (obj.GetType() != GetType())
+				return false;
 
-            var other = (Coverage)obj;
-            return this.SequenceEqual(other);
-        }
+			var other = (Coverage)obj;
+			return this.SequenceEqual(other);
+		}
 
-        public override int GetHashCode()
-        {
-            if (Count > 0)
-                return this.First().GetHashCode();
-            else
-                return GetHashCode();
-        }
+		public override int GetHashCode()
+		{
+			if (Count > 0)
+				return this.First().GetHashCode();
+			else
+				return GetHashCode();
+		}
 
-        #endregion
+		#endregion
 
-    }
+	}
 }
--- src/Karno/Group.cs
+++ src/Karno/Group.cs
@@ -3,44 +3,44 @@ using System.Linq;
 
 namespace Karno
 {
-    public class Group : SortedSet<string>
-    {
+	public class Group : SortedSet<string>
+	{
 
-        public Group(IEnumerable<string> collection) : base(collection)
-        {
+		public Group(IEnumerable<string> collection) : base(collection)
+		{
 
-        }
+		}
 
-        public Group() : base()
-        {
+		public Group() : base()
+		{
 
-        }
+		}
 
-        public bool? IsEssential { get; set; }
+		public bool? IsEssential { get; set; }
 
-        #region Equality Comparison
+		#region Equality Comparison
 
-        public override bool Equals(object obj)
-        {
-            if (obj.GetType() != GetType())
-                return false;
+		public override bool Equals(object obj)
+		{
+			if (obj.GetType() != GetType())
+				return false;
 
-            var other = (Group)obj;
-            return this.SequenceEqual(other);
-        }
+			var other = (Group)obj;
+			return this.SequenceEqual(other);
+		}
 
-        public override int GetHashCode()
-        {
-            // This is the reason we use a SortedList for the "Group" object, because we need to create as many buckets
-            // as there are distinct first elements of each groups
-            if (Count > 0)
-                return this.First().GetHashCode();
-            else
-                return GetHashCode();
-        }
+		public override int GetHashCode()
+		{
+			// This is the reason we use a SortedList for the "Group" object, because we need to create as many buckets
+			// as there are distinct first elements of each groups
+			if (Count > 0)
+				return this.First().GetHashCode();
+			else
+				return GetHashCode();
+		}
 
-        #endregion
+		#endregion
 
-    }
+	}
 
 }
